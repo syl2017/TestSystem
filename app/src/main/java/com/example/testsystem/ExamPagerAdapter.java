ed25abfbb0afcf52c4779613b1f64a16ebc0ea13
18056074197 <18056074197@163.com>
2019-12-18 16:59:38 +0800

    完成ViewPager数据更新

--- app/src/main/java/com/example/testsystem/ExamPagerAdapter.java
+++ app/src/main/java/com/example/testsystem/ExamPagerAdapter.java
@@ -7,6 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.testsystem.bean.QuestionBean;
+
+import org.litepal.crud.DataSupport;
+import org.w3c.dom.Text;
+
 import java.util.List;
 
 /**
@@ -17,25 +22,40 @@ import java.util.List;
 public class ExamPagerAdapter extends PagerAdapter {
 
     private Context mContext;
-    private List<String> mData;
+    private List<?> all;
     private TextView testQuestion;
+    private TextView text_a;
+    private TextView text_b;
+    private TextView text_c;
+    private TextView text_d;
 
-    public ExamPagerAdapter(Context context, List<String> list) {
+    public ExamPagerAdapter(Context context, List<?> list) {
         mContext = context;
-        mData = list;
+        all = list;
+
     }
 
     @Override
     public int getCount() {
-        return mData.size();
+        return 3;
     }
 
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         View view = View.inflate(mContext, R.layout.examing_viewpager_item, null);
+
         testQuestion = (TextView) view.findViewById(R.id.activity_prepare_test_question);
-        testQuestion.setText(mData.get(position));
+        text_a = (TextView) view.findViewById(R.id.vote_submit_select_text_a);
+        text_b = (TextView) view.findViewById(R.id.vote_submit_select_text_b);
+        text_c = (TextView) view.findViewById(R.id.vote_submit_select_text_c);
+        text_d = (TextView) view.findViewById(R.id.vote_submit_select_text_d);
+        testQuestion.setText(((QuestionBean)all.get(position)).getLabel()+((QuestionBean)all.get(position)).getQuestion());
+        text_a.setText(((QuestionBean) all.get(position)).getOption_a());
+        text_b.setText(((QuestionBean) all.get(position)).getOption_b());
+        text_c.setText(((QuestionBean) all.get(position)).getOption_c());
+        text_d.setText(((QuestionBean) all.get(position)).getOption_d());
+
         container.addView(view);
         return view;
 
--- app/src/main/java/com/example/testsystem/ViewPageActivity.java
+++ app/src/main/java/com/example/testsystem/ViewPageActivity.java
@@ -5,12 +5,15 @@ import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.testsystem.bean.QuestionBean;
 
 import org.litepal.crud.DataSupport;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -24,21 +27,34 @@ public class ViewPageActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
     private DBHelper dbHelper;
+    private ArrayList<String> list;
+    private TextView activityPrepareTestQuestion;
+    private int viewpagerIndex=0;
+    private ArrayList<View> viewpagerList;
+    private List<QuestionBean> all;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.viewpager);
 
-        dbHelper =new DBHelper(this);
-        QuestionBean first = DataSupport.findFirst(QuestionBean.class);
-        Log.d(TAG, "onCreate: "+first);
+        dbHelper = new DBHelper(this);
+        all = DataSupport.findAll(QuestionBean.class);
+//        QuestionBean first = DataSupport.findFirst(QuestionBean.class);
+//        Log.d(TAG, "onCreate: " + first);
+//        list = new ArrayList<>();
+//        for (int i = 0; i < 3; i++) {
+//            list.add("第一题" + i);
+//        }
+        viewpagerList = new ArrayList<>();
+        init();
+    }
+
+    private void init() {
         mViewPager = (ViewPager) findViewById(R.id.viewPager);
-        ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            list.add("第一题" + i);
-        }
-        mViewPager.setAdapter(new ExamPagerAdapter(this,list));
+        mViewPager.setAdapter(new ExamPagerAdapter(this, all));
+//        activityPrepareTestQuestion = findViewById(R.id.activity_prepare_test_question);
+//        mViewPager.setCurrentItem(viewpagerIndex);
     }
 
 }
